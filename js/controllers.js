ea766fcded25827877580b2534206d1726d82bdc
Samaksh <samaksh@bifortis.com>
2015-03-13 14:37:20 +0530

    .

--- js/controllers.js
+++ js/controllers.js
@@ -1,10 +1,47 @@
 angular.module('starter.controllers', [])
 
 
-.controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+.controller('AccountCtrl', function($scope, itemList) {
+	$scope.itemlist = itemList.all();
+	$scope.remove = function(item) {
+	itemList.remove(item); 
+	};
+	$scope.dealToggle = false;
+	$scope.searchClicked = false;
+	$scope.categories =['cat1',
+						'cat2',
+						'cat3',
+						'cat4', 
+						'cat5', 
+						'cat6'];
+	$scope.toggleSearch = function(){
+		$scope.searchClicked = !($scope.searchClicked);
+		$scope.searchText = null;
+	};
+	// toggle selectedCategories 
+	$scope.toggleSelected = function(cat){
+		if ($scope.selectedCategories.indexOf(cat) !== -1){
+			$scope.selectedCategories.pop(cat);
+		}
+		else {
+			$scope.selectedCategories.push(cat);
+		};
+	};
+	$scope.selectedCategories = [];
+	$scope.filterByCat = function(image) {
+		if ($scope.selectedCategories.length > 0) {
+		return ($scope.selectedCategories.indexOf(image.category) !== -1);
+		}
+		else {
+		return true;
+		}
+	};
+	
+})
+.controller('ItemCtrl', function($scope, $stateParams, itemList){
+	console.log("paprika");
+	$scope.item = itemList.get($stateParams.itemId);
+
 })
 .controller('LoginCtrl', function(){
 
@@ -17,7 +54,7 @@ angular.module('starter.controllers', [])
 
 })
 .controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
-  $scope.chat = Chats.get($stateParams.chatID);
+  $scope.chat = Chats.get($stateParams.chatId);
 })
 
 .controller('messageCtrl', function($scope, Chats){
--- js/services.js
+++ js/services.js
@@ -1,6 +1,6 @@
 angular.module('starter.services', [])
 
-.factory('Chats', function() {
+.factory('Chats', function () {
   // Might use a resource here that returns a JSON array
 
   // Some fake testing data
@@ -32,16 +32,63 @@ angular.module('starter.services', [])
   }];
 
   return {
-    all: function() {
+    all: function () {
       return chats;
     },
-    remove: function(chat) {
+    remove: function (chat) {
       chats.splice(chats.indexOf(chat), 1);
     },
-    get: function(chatId) {
+    get: function (chatId) {
       for (var i = 0; i < chats.length; i++) {
         if (chats[i].id === parseInt(chatId)) {
           return chats[i];
+        }
+      }
+      return null;
+    }
+  };
+})
+
+.factory('itemList', function (){
+    var itemlist = [
+                      {
+                        id : 0,
+                        imgurl:'https://dtex4kvbppovt.cloudfront.net/images/1ddd27484ee94475bff83cf81d2bc50d7762508c.13811.png',
+                        category : 'cat1',
+                        name :'free',
+                        desc : 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque volutpat id nibh eget convallis. Pellentesque sit amet ultricies ipsum. Suspendisse nec blandit ex, non dictum ipsum. Nulla aliquet tristique hendrerit. Quisque dapibus est faucibus libero elementum cursus. Morbi consectetur augue ut eros malesuada convallis. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae'
+                      }, {
+                        id : 1,
+                        imgurl:'https://dtex4kvbppovt.cloudfront.net/images/720121e7462d8c7863b4dd8fa7b5c1089b5f5fb2.33862.png', 
+                        category : 'cat2',
+                        name : 'discount',
+                        desc : 'Suspendisse sed malesuada tortor. Fusce vitae interdum augue. Quisque vulputate luctus mauris, ut viverra dui mollis id. Nullam pulvinar mollis aliquam. Nam vulputate massa ac metus ornare suscipit. Quisque nunc ex, gravida et massa porta, sagittis luctus est.'
+                      }, {
+                        id : 2,
+                        imgurl:'https://dtex4kvbppovt.cloudfront.net/images/877f1c561e735f7b9f419ff9ac79eb8c7481119d.16744.png', 
+                        category : 'pat3',
+                        name : 'gift',
+                        desc : 'Phasellus in diam gravida, egestas lorem tincidunt, tempus metus. Phasellus sit amet nulla id lacus faucibus commodo at eu metus. Proin metus risus, venenatis id tincidunt at, porta accumsan diam. Proin posuere mattis leo, in auctor orci tincidunt vel. '
+                      }, {
+                        id : 3,
+                        imgurl:'https://dtex4kvbppovt.cloudfront.net/images/d11ba0b3095bb19d8092cd29be9cbb9e197671ea.28088.png', 
+                        category : 'cat4',
+                        name : 'prize',
+                        desc : 'Pellentesque suscipit fringilla eros, in dictum sapien tempor et. Phasellus metus massa, pulvinar aliquam ante et, vulputate pulvinar lorem. Phasellus iaculis eget justo in blandit. Aliquam quis ipsum sed nulla pellentesque pharetra quis nec metus. Fusce non vestibulum nunc.'
+                      }
+                    ];
+
+  return {
+    all : function (){
+      return itemlist;
+    },
+    remove :  function(item){
+      itemlist.splice(itemlist.indexOf(item),1);
+    },
+    get : function (itemId){
+      for (var i = 0; i<itemlist.length; i++) {
+        if (itemlist[i].id === parseInt(itemId)){
+          return itemlist[i];
         }
       }
       return null;
